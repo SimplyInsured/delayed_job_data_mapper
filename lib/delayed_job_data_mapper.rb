3b380db53e96cc91d8bc711923ca3b65dcf48342
Charles Szymanski <charles@simplyinsured.com>
2016-10-18 15:55:52 -0700

    Get rid of development dependencies

--- lib/delayed_job_data_mapper.rb
+++ lib/delayed_job_data_mapper.rb
@@ -1,9 +1,6 @@
 # encoding: utf-8
 require 'dm-core'
 require 'dm-mysql-adapter'
-require "pry"
-require "pry-byebug"
-require "byebug"
 require 'dm-active_model' # for persisted? alias for DJ 4.0.1
 require 'delayed_job'
 require 'delayed/serialization/data_mapper'
